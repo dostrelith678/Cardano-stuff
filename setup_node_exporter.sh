403dff8846dfd556fbccc346d495611a749036dd
Damjan Ostrelič <damjan.ostrelic@gmail.com>
2020-08-09 10:36:51 +0100

    Create setup_node_exporter.sh

---
+++ setup_node_exporter.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+######################################################################
+#### Environment Variables
+######################################################################
+CNODE_IP=127.0.0.1
+NEXP_PORT=9091
+NEXP_DIR="/tmp/monitoring/node_exporter-1.0.1.linux-amd64"
+
+#get node_exporter to tmp directorty
+cd /tmp
+mkdir monitoring
+cd monitoring
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz
+
+#start NODEEXP
+#cd node_exporter-1.0.1.linux-amd64
+#./node_exporter
+
+#create systemd service
+sudo touch /etc/systemd/system/node_exporter.service
+cat > /etc/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=$(whoami)
+Restart=on-failure
+ExecStart=$NEXP_DIR/node_exporter --web.listen-address="$CNODE_IP:$NEXP_PORT"
+WorkingDirectory=$NEXP_DIR
+LimitNOFILE=3500
+[Install]
+WantedBy=default.target
+EOF
+
+#restart daemon and enable service
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+
+echo -e "
+=====================================================
+Installation is completed
+=====================================================
+Node exp metrics:   http://$CNODE_IP:$NEXP_PORT
+"
