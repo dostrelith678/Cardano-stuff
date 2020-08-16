ea4e895e3fbca3bd4387c030a98b388ec7b089a5
Damjan Ostrelič <damjan.ostrelic@gmail.com>
2020-08-16 08:00:50 +0100

    add sleep times

--- cnode-restart.sh
+++ cnode-restart.sh
@@ -2,14 +2,14 @@
 #Kill and restart node
 
 killall cardano-node
-sleep 5
+sleep 2
 
 #kill tmux session
 tmux kill-session -t relay
-
+sleep 2
 #restart node
 SCRIPT_PATH="/opt/cardano/cnode/scripts/cnode.sh"
 
 tmux new -s "relay" -d
+sleep 2
 tmux send-keys -t "relay" "$SCRIPT_PATH" C-m
-tmux attach -t "relay" -d
